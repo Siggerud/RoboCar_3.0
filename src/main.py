c16c4fedf053b6e2503e458126d7397348115823
Siggerud <christian.siggerud@gmail.com>
2024-11-27 20:49:45 +0100

    debug

--- src/main.py
+++ src/main.py
@@ -147,7 +147,7 @@ if servoVertical:
 
 #if arduinoCommunicator:
     #carController.add_arduino_communicator(arduinoCommunicator)
-
+"""
 if camera:
     cameraHelper = CameraHelper()
     cameraHelper.add_car(car)
@@ -155,7 +155,7 @@ if camera:
 
     carController.add_camera(camera)
     carController.add_camera_helper(cameraHelper)
-
+"""
 # start car
 carController.start()
 
