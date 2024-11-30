7be002a72ec34d7ee9a81f7f541f8ea7e629e622
Siggerud <christian.siggerud@gmail.com>
2024-11-30 15:53:54 +0100

    bug fix

--- src/audioManager.py
+++ src/audioManager.py
@@ -10,11 +10,11 @@ class AudioManager:
     def listen_for_command(self):
         r = sr.Recognizer()
         with sr.Microphone(device_index=self._deviceIndex) as source:
-            r.adjust_for_ambient_noise(source)
+            #r.adjust_for_ambient_noise(source)
             audio_text = r.listen(source, timeout=None, phrase_time_limit=self._listenTime)
             try:
                 # using google speech recognition
-                spokenWords: str = self._recognizer.recognize_google(audio_text)
+                spokenWords: str = r.recognize_google(audio_text)
                 print("Text: " + spokenWords)
             except sr.UnknownValueError:
                 print("Couldn't understand that")
