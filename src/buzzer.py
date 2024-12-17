a4afb931d623c4c740db4bd674b8c7b1ecf26322
Siggerud <christian.siggerud@gmail.com>
2024-12-17 21:38:47 +0100

    added possibility to honk for a specified time

--- src/buzzer.py
+++ src/buzzer.py
@@ -5,21 +5,38 @@ class Buzzer:
     def __init__(self, buzzerPin: int):
         self._buzzerPin: int = buzzerPin
         self._defaultHonkTime = 0.3 #TODO: add this to config
+        self._maxHonkTime = 2 #TODO: add this to config
         self._buzzCommand: dict = {"start horn": {"description": "starts honking"}}
+        self._buzzForSpecifiedTimeCommands: dict = self._set_honk_for_specified_time_commands()
 
     def setup(self):
         GPIO.setup(self._buzzerPin, GPIO.OUT)
 
     def handle_voice_command(self, command):
         if command in self._buzzCommand:
-            self._buzz()
+            honkTime = self._defaultHonkTime
+        elif command in self._buzzForSpecifiedTimeCommands:
+            honkTime = self._buzzForSpecifiedTimeCommands[command]
+
+        self._buzz(honkTime)
 
     def get_voice_commands(self) -> list[str]:
         return list(self._buzzCommand.keys())
 
-    def _buzz(self):
+    def _buzz(self, honkTime):
         GPIO.output(self._buzzerPin, GPIO.HIGH)
-        sleep(self._defaultHonkTime)
+        sleep(honkTime) 
         GPIO.output(self._buzzerPin, GPIO.LOW)
+
+    def _set_honk_for_specified_time_commands(self) -> dict:
+        honkTime: float = 0.1
+        stepValue: float = 0.1
+        honkCommands: dict = {}
+        while honkTime <= self._maxHonkTime:
+            honkCommands[f"start horn {round(honkTime, 1)}"] = round(honkTime, 1) # round honkTime to avoid floating numbers with many decimals
+
+            honkTime += stepValue
+
+        return honkCommands
 
 
