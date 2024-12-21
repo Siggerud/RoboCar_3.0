963645bbafe67d0d0217f53f5bd658306b7c38fb
Siggerud <christian.siggerud@gmail.com>
2024-12-21 12:49:51 +0100

    debug

--- src/servoHandling.py
+++ src/servoHandling.py
@@ -55,7 +55,7 @@ class ServoHandling:
                 "pwmValue": self._angleToPwmValues[self._minAngles["horizontal"]]
             }
         }
-
+        print(self._lookOffsetCommands)
         self._lookCenterCommand: dict = {
             userCommands["lookCenterCommand"]: {
                 "description": "Centers camera"
