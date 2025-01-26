6076d433a4d4d15764f88f545a6ad48e285135e1
Siggerud <christian.siggerud@gmail.com>
2025-01-26 16:40:44 +0100

    added calculation of angles

--- src/stabilizer.py
+++ src/stabilizer.py
@@ -1,5 +1,6 @@
 from mpu6050 import mpu6050
 from time import sleep
+from math import atan, pi
 
 class Stabilizer:
     def __init__(self, address: int = 0x68):
@@ -7,21 +8,24 @@ class Stabilizer:
 
     def stabilize(self):
         # Read the sensor data
-        accelerometer_data = self._mpu6050.get_accel_data()
+        accelerometer_data = self._mpu6050.get_accel_data(g=True) # get value in gravity units
 
         # Print the sensor data
-        if accelerometer_data["x"] > 0.5:
-            print("tilting car backward")
-        elif accelerometer_data["x"] < -0.5:
-            print("tilting car forward")
+        xAccel = self._set_value_equal_to_1_if_greater(accelerometer_data["x"])
+        yAccel = self._set_value_equal_to_1_if_greater(accelerometer_data["y"])
+        zAccel = self._set_value_equal_to_1_if_greater(accelerometer_data["z"])
 
-        if accelerometer_data["y"] > 0.5:
-            print("tilting car to the right")
-            print(accelerometer_data["y"])
-        elif accelerometer_data["y"] < -0.5:
-            print("tilting car to the left")
-            print(accelerometer_data["y"])
+        # Calculate the angle and convert from radian to degrees
+        xAngle = atan(xAccel / zAccel) * 180 / pi
+        yAngle = atan(yAccel / zAccel) * 180 / pi
 
+        print(f"xAngle: {xAngle}, yAngle: {yAngle}")
 
         # Wait for 1 second
         sleep(1)
+
+    def _set_value_equal_to_1_if_greater(self, accelValue):
+        if accelValue > 1:
+            return 1
+        return accelValue
+
