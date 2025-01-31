d9d72179037e3612de45e9a2f6c71cdf89fb403d
Siggerud <christian.siggerud@gmail.com>
2025-01-31 22:50:09 +0100

    bug fix

--- test/testAudioHandler.py
+++ test/testAudioHandler.py
@@ -8,7 +8,7 @@ from unittest.mock import Mock, patch
 
 def test_bluetooth_headphones_connected():
     with pytest.raises(MicrophoneException):
-        with patch('subprocess.check_output', return_value="Device 12345 myMicrophone"), \
+        with patch('subprocess.check_output', return_value=b"Device 12345 myMicrophone"), \
         patch.object(AudioHandler, '_get_device_index', return_value=1):
             audioHandler = AudioHandler("stop", "English (United States)", "myMicrophone")
 
