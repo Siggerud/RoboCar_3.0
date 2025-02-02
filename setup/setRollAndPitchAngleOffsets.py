a7ed0f073c5e12772cc001935ac99f2ba26f1d27
Siggerud <christian.siggerud@gmail.com>
2025-02-02 17:06:27 +0100

    tidying up

--- setup/setRollAndPitchAngleOffsets.py
+++ setup/setRollAndPitchAngleOffsets.py
@@ -9,14 +9,14 @@ answer: str = input("Press y when car is on horizontal surface, and q to quit\n"
 if answer == "q":
     print("Quitting setup of offset angles")
     exit()
-
+#TODO: add validition checks of answers
 print("\nGetting offset angles...")
 xAccel: float = mpu.get_accel_data()["x"]
 yAccel: float = mpu.get_accel_data()["y"]
 zAccel: float = mpu.get_accel_data()["z"]
 
-offsetX: float  = atan(xAccel / zAccel) / 2 / pi * 360
-offsetY: float  = atan(yAccel / zAccel) / 2 / pi * 360
+offsetX: float  = round(atan(xAccel / zAccel) / 2 / pi * 360, 3)
+offsetY: float  = round(atan(yAccel / zAccel) / 2 / pi * 360, 3)
 
 print(f"Found offsets: x-axis {offsetX}°, y-axis {offsetY}")
 answer: str = input("Do you want to write these values to the config file? (y/n)\n")
@@ -27,8 +27,8 @@ if answer == "n":
 print("\nWriting offsets to config file...")
 config = ConfigParser()
 config.read("../src/config.ini")
-config["Stabilizer"]["offset_x"] = str(round(offsetX, 3))
-config["Stabilizer"]["offset_y"] = str(round(offsetY, 3))
+config["Stabilizer"]["offset_x"] = str(offsetX)
+config["Stabilizer"]["offset_y"] = str(offsetY, 3)
 with open("../src/config.ini", "w") as configFile:
     config.write(configFile)
 print(f"offset_x value set to {offsetX} and offset_y value set to {offsetY}")
