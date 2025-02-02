620cd82bb01771dc31c19104c1dfc4cca0711db7
Siggerud <christian.siggerud@gmail.com>
2025-02-02 17:30:53 +0100

    testing latest changes

--- src/stabilizer.py
+++ src/stabilizer.py
@@ -29,6 +29,8 @@ class Stabilizer:
         self._confidenceFactor: float = 0.92
         self._errorFactor: float = 0.01
 
+        self._count = 0
+
     def stabilize(self):
         tStart: float = time()
         # Read the sensor data
@@ -62,8 +64,9 @@ class Stabilizer:
         # calculate the steady state error values
         self._errorRoll = self._errorRoll + (self._rollAccelAngle - self._rollComp) * self._tLoop
         self._errorPitch = self._errorPitch + (self._pitchAccelAngle - self._pitchComp) * self._tLoop
-
-        print(f"rollA: {round(self._rollAccelAngle, 2)}, rollC: {round(self._rollComp, 2)}, pitchA: {round(self._pitchAccelAngle, 2)}, pitchC: {round(self._pitchComp, 2)}")
+        self._count += 1
+        if self._count % 50 == 0:
+            print(f"rollA: {round(self._rollAccelAngle, 2)}, rollC: {round(self._rollComp, 2)}, pitchA: {round(self._pitchAccelAngle, 2)}, pitchC: {round(self._pitchComp, 2)}")
 
         tStop: float = time()
         self._tLoop = tStop - tStart
