885dd7c1e2b8917a3f325e22788fbe2faa936945
Siggerud <christian.siggerud@gmail.com>
2025-02-06 20:27:07 +0100

    removed init files and added a setup method

--- test/testBuzzer.py
+++ test/testBuzzer.py
@@ -1,5 +1,14 @@
-import pytest
-from src.buzzer import Buzzer
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-def test_get_command_validity():
+import pytest
+from buzzer import Buzzer
+
+@pytest.fixture
+def buzzer():
+    return Buzzer(1, 0.3, 4, {"buzzCommand": "start honking", "buzzForSpecifiedTimeCommand": "honk {param}"})
+
+def test_get_command_validity(buzzer):
+    honker = buzzer
     pass
