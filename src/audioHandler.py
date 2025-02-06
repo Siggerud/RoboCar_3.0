eef9000ffa64fc7087bc43c73a6604423421e10b
Siggerud <christian.siggerud@gmail.com>
2025-02-06 19:52:35 +0100

    experimenting...

--- src/audioHandler.py
+++ src/audioHandler.py
@@ -42,13 +42,12 @@ class AudioHandler:
 
                 spokenWords = self._clean_up_spoken_words(spokenWords)
 
-                # set flag value to true if command is exit command
-                if spokenWords == self._exitCommand:
-                    flag.value = True
-                    break
-
                 # set the command in IPC
                 self._queue.put(spokenWords)
+
+                # set flag value to true if command is exit command
+                if spokenWords == self._exitCommand:
+                    break
 
                 sleep(0.3)
 
