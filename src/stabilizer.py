135a9497731124ae92c8b8ef11c6394687af3f84
Siggerud <christian.siggerud@gmail.com>
2025-02-11 20:27:22 +0100

    debug

--- src/stabilizer.py
+++ src/stabilizer.py
@@ -3,12 +3,14 @@ from motionTrackingDevice import MotionTrackingDevice
 class Stabilizer:
     def __init__(self, motionTrackingDevice: MotionTrackingDevice):
         self._motionTrackingDevice = motionTrackingDevice
+        self._count = 0
 
     def stabilize(self):
+        self._count += 1
         rollAngle, pitchAngle = self._motionTrackingDevice.get_roll_and_pitch()
-
-        print(f"Roll angle: {rollAngle}")
-        print(f"Pitch angle: {pitchAngle}")
+        if self._count % 50 == 0:
+            print(f"Roll angle: {rollAngle}")
+            print(f"Pitch angle: {pitchAngle}")
 
 
 
