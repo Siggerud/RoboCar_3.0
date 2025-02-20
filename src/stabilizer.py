8faf96350ca5d543eac8c99ce5b4090a98edd93d
Siggerud <christian.siggerud@gmail.com>
2025-02-20 19:07:27 +0100

    experimenting

--- src/stabilizer.py
+++ src/stabilizer.py
@@ -6,6 +6,8 @@ class Stabilizer:
         self._rollTreshold = rollTreshold
         self._pitchTreshold = pitchTreshold
         self._count = 0
+        self._overRollTreshold = False
+        self._overPitchTreshold = False
 
     def stabilize(self):
         self._count += 1
@@ -13,9 +15,18 @@ class Stabilizer:
         if self._count % 10 == 0:
             print(f"Roll angle: {rollAngle}, Pitch angle: {pitchAngle}")
         if abs(rollAngle) > self._rollTreshold:
-            print("Roll angle is too high")
-        elif abs(pitchAngle) > self._pitchTreshold:
-            print("Pitch angle is too high")
+            if overRollTreshold == False:
+                print("Roll angle is too high")
+                self._overRollTreshold = True
+        else:
+            self._overRollTreshold = False
+        if abs(pitchAngle) > self._pitchTreshold:
+            if overPitchTreshold == False:
+                print("Pitch angle is too high")
+                self._overPitchTreshold = True
+        else:
+            self._overPitchTreshold = False
+
 
 
 
