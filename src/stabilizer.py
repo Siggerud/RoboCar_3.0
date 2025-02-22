75913cd869d6a9eb0a2cf8fc647f64dc9305b791
Siggerud <christian.siggerud@gmail.com>
2025-02-22 13:45:57 +0100

    debugging

--- src/stabilizer.py
+++ src/stabilizer.py
@@ -18,8 +18,8 @@ class Stabilizer:
     def stabilize(self):
         self._count += 1
         rollAngle, pitchAngle = self._motionTrackingDevice.get_roll_and_pitch()
-        #if self._count % 50 == 0:
-        #    print(f"Roll angle: {rollAngle}, Pitch angle: {pitchAngle}")
+        if self._count % 200 == 0:
+            print(f"Roll angle: {rollAngle}, Pitch angle: {pitchAngle}")
         #    print(f"Max roll: {self._maxRoll}, Max pitch: {self._maxPitch}")
         #    print()
 
